b6ef9b74aee12af849f39343a7990c86c6eb9afa
eylulmeldacakmak <eylulmeldacakmak@posta.mu.edu.tr>
2025-04-18 12:15:52 +0300

    Add files via upload

---
+++ week8-eylulmeldacakmak/Shape.java
@@ -0,0 +1,34 @@
+public class Shape {
+
+    private String color = "white";
+
+    public Shape(){
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+/*
+    public void describe(int sideA, int sideB){
+        System.out.println("Side A is " + sideA + "Side B is " + sideB);
+    }
+
+    public void describe(int sideA){
+        System.out.println("Side A is " + sideA);
+    }
+
+    public void describe(double sideA){
+        System.out.println("Radius is " + sideA);
+    }
+
+ */
+
+    public void describe(){
+        System.out.println("This is a shape and " + "it's color is " + color);
+    }
+}
